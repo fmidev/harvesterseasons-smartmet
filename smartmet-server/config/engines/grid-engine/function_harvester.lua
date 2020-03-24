800400a4f9087b7b2da5cdf9970c09fef04f7d81
Mikko Strahlendorff <mstrahl@gmail.com>
2020-03-24 06:09:17 +0000

    added ssl-proxy container to setup for https support, added HARVIDX lua script for function to calculate harvestability idx on the fly for a given parameter and threshold

---
+++ smartmet-server/config/engines/grid-engine/function_harvester.lua
@@ -0,0 +1,134 @@
+ParamValueMissing = -16777216;
+debug = 0;
+
+-- ###########################################################################################
+-- FUNCTION for and index if ensemble members are to 90 % over a threshold or only 10 %?
+--  The function returns 0 for only 10% are over, 1 for between 10 and 90 % and 2 for over 90%
+-- ###########################################################################################
+
+function HARVIDX(numOfParams,params)
+  local result = {};
+  if (debug == 1) then
+    for index, value in pairs(params) do
+      print(index.." : "..value);
+    end
+  end
+  if (numOfParams > 0) then 
+    local count = 0;
+    local agree = 0;
+    local disagree = 0;
+    local threshold = params[1];
+    for index, value in pairs(params) do
+      if (value ~= ParamValueMissing and index > 1) then
+        if (value > threshold) then
+	 agree = agree + 1;
+	 count = count + 1;
+	else
+	 disagree = disagree + 1;
+ 	 count = count + 1;
+        end
+      end
+    end
+    result.message = 'OK';
+--    result.value = agree;
+    if (agree / count >= 0.9) then
+       result.value = 2;
+    elseif (agree / count <= 0.1) then
+    	result.value = 0;
+    else
+	result.value = 1;
+    end
+  else
+    result.message = 'OK';
+    result.value = ParamValueMissing;
+  end
+
+  return result.value,result.message;
+end
+
+
+-- ***********************************************************************
+--  FUNCTION : getFunctionNames
+-- ***********************************************************************
+--  The function returns the list of available functions in this file.
+--  In this way the query server knows which function are available in
+--  each LUA file.
+-- 
+--  Each LUA file should contain this function. The 'type' parameter
+--  indicates how the current LUA function is implemented.
+--
+--    Type 1 : 
+--      Function takes two parameters as input:
+--        - numOfParams => defines how many values is in the params array
+--        - params      => Array of float values
+--      Function returns two parameters:
+--        - result value (function result or ParamValueMissing)
+--        - result string (=> 'OK' or an error message)
+--
+--    Type 2 : 
+--      Function takes three parameters as input:
+--        - columns       => Number of the columns in the grid
+--        - rows          => Number of the rows in the grid
+--        - params        => Grid values (= Array of float values)
+--      Function return one parameter:
+--        - result array  => Array of float values (must have the same 
+--                           number of values as the input 'params'.               
+--
+--    Type 3 : 
+--      Function takes four parameters as input:
+--        - columns       => Number of the columns in the grid
+--        - rows          => Number of the rows in the grid
+--        - params1       => Grid 1 values (= Array of float values)
+--        - params2       => Grid 2 values (= Array of float values)
+--      Function return one parameter:
+--        - result array  => Array of float values (must have the same 
+--                           number of values as the input 'params1'.               
+--  
+--    Type 4 : 
+--      Function takes five parameters as input:
+--        - columns       => Number of the columns in the grid
+--        - rows          => Number of the rows in the grid
+--        - params1       => Grid 1 values (= Array of float values)
+--        - params2       => Grid 2 values (= Array of float values)
+--        - params3       => Grid point angles to latlon-north (= Array of float values)
+--      Function return one parameter:
+--        - result array  => Array of float values (must have the same 
+--                           number of values as the input 'params1'.
+--      Can be use for example in order to calculate new Wind U- and V-
+--      vectors when the input vectors point to grid-north instead of
+--      latlon-north.               
+--
+--    Type 5 : 
+--      Function takes three parameters as input:
+--        - language    => defines the used language
+--        - numOfParams => defines how many values is in the params array
+--        - params      => Array of float values
+--      Function return two parameters:
+--        - result value (string)
+--        - result string (=> 'OK' or an error message)
+--      Can be use for example for translating a numeric value to a string
+--      by using the given language.  
+--  
+--    Type 6 : 
+--      Function takes two parameters as input:
+--        - numOfParams => defines how many values is in the params array
+--        - params      => Array of string values
+--      Function return one parameters:
+--        - result value (string)
+--      This function takes an array of strings and returns a string. It
+--      is used for example in order to get additional instructions for
+--      complex interpolation operations.  
+--  
+-- ***********************************************************************
+
+function getFunctionNames(type)
+
+  local functionNames = '';
+
+  if (type == 1) then 
+    functionNames = 'HARVIDX';
+  end
+  
+  return functionNames;
+
+end
