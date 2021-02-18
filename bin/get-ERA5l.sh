88e070c54316c1ce962223e1fce7f6b732e0be7c
Mikko Strahlendorff <mstrahl@gmail.com>
2021-02-18 07:31:03 +0000

    many updates

--- bin/get-ERA5l.sh
+++ bin/get-ERA5l.sh
@@ -19,5 +19,5 @@ cds-era5l.py $year $month
 #grib_set -s edition=2 ec-sf-$year$month-all-24h.grib grib/EC-SF-${year}${month}01T0000-all-24h.grib2
 #cdo sellonlatbox,-22,45,27,72 ec-sf-$year$month-all-24h.grib EC-SF_$year${month}01T0000_all-24h-euro+y.grib
 #grib_set  -s jScansPositively=0,numberOfForecastsInEnsemble=51 -w jScansPositively=1,numberOfForecastsInEnsemble=0 EC-SF_$year${month}01T0000_all-24h-euro+y.grib grib/EC-SF_$year${month}01T0000_all-24h-euro.grib
-mv era5-$year$month-sfc-1h.grib grib/ERA5L_$year${month}01T000_euro_1h.grib
+mv era5l-$year$month-sfc-12h.grib grib/ERA5L_$year${month}01T000_euro_1h.grib
 #sudo docker exec smartmet-server /bin/fmi/filesys2smartmet /home/smartmet/config/libraries/tools-grid/filesys-to-smartmet.cfg 0
--- bin/get-fmi-wf.sh
+++ bin/get-fmi-wf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
+conda activate xr
 if [ $# -ne 0 ]
 then
     date=$1
@@ -16,12 +17,14 @@ timestep=1440
 wget -O /home/smartmet/data/fmi-smartmet-$date-e+fire.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=Evaporation,ForestGroundHumidity,ForestFireWarning&timestep=$timestep&starttime=${date}T000000&format=grib2&producer=metsapalomalli"
 #wget -O /home/smartmet/data/fmi-smartmet-$date-t+rr.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=Temperature,Precipitation1h&timestep=60&starttime=${date}T000000&format=grib1&producer=pal_skandinavia"
 wget -O /home/smartmet/data/fmi-smartmet-$date-24h.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=MinimumTemperature24h,MaximumTemperature24h,DailyMeanTemperature,Precipitation24h&timestep=$timestep&starttime=${date}T000000&format=grib2&producer=daily00"
-wget -O /home/smartmet/data/fmi-smartmet-$date-swvl2.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=VolumetricSoilWaterLayer28&timestep=720&starttime=${date}T000000&format=grib2&producer=fmi_soil_water&projection=epsg:4326"
+# SWVL2 calculation only for summer season
+#wget -O /home/smartmet/data/fmi-smartmet-$date-swvl2.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=VolumetricSoilWaterLayer28&timestep=720&starttime=${date}T000000&format=grib2&producer=fmi_soil_water&projection=epsg:4326"
 wget -O /home/smartmet/data/fmi-smartmet-$date-TG.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=GroundTemperature&timestep=$timestep&starttime=${date}T000000&format=grib2&producer=pal_skandinavia_frost"
 #wget -O /home/smartmet/data/fmi-smartmet-$date-rr6h.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=Precipitation6h&timestep=360&starttime=${date}T000000&format=grib1&producer=pal_skandinavia_precipitation6h"
 wget -O /home/smartmet/data/fmi-smartmet-$date-snowacc.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=WaterAccumulation,SnowAccumulation,SnowWaterRatio&timestep=720&starttime=${date}T000000&format=grib2&producer=snow_accumulation"
 # observations
-wget -O /home/smartmet/data/fmi-smartmet-$ydate-swvl2-cum.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=VolumetricSoilWaterLayer28&timestep=1440&starttime=${ydate:0:4}0401T090000&format=grib2&producer=fmi_soil_cumulative&projection=epsg:4326"
+# SWVL2 calculation only for summer season
+#wget -O /home/smartmet/data/fmi-smartmet-$ydate-swvl2-cum.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=VolumetricSoilWaterLayer28&timestep=1440&starttime=${ydate:0:4}0401T090000&format=grib2&producer=fmi_soil_cumulative&projection=epsg:4326"
 wget -O /home/smartmet/data/fmi-smartmet-$ydate-synop-krg.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=Temperature,Evaporation,WindSpeedMS&timestep=data&starttime=${ydate}T000000&format=grib1&producer=kriging_suomi_synop&projection=epsg:4326"
 wget -O /home/smartmet/data/fmi-smartmet-$ydate-sd-krg.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=WaterEquivalentOfSnow&timestep=data&starttime=${ydate}T000000&format=grib1&producer=kriging_suomi_snow&projection=epsg:4326"
 wget -O /home/smartmet/data/fmi-smartmet-$ydate-tg-krg.grib "http://data.fmi.fi/fmi-apikey/$fmiapi/download?param=GroundTemperature&timestep=data&starttime=${ydate}T000000&format=grib2&producer=roadkriging_suomi&projection=epsg:4326"
@@ -40,6 +43,6 @@ cdo --eccodes -O chparam,144.173.192,11.1.0 -mulc,0.001 -add grib/SMARTMET_${eda
 cdo --eccodes chparam,144.173.192,11.1.0 -add -mulc,0.001 grib/SMARTMET_${date:0:6}01T0000_${date}T0000_sfara.grib \
     -seltimestep,8 sde-state.grib grib/SMARTMET_${date:0:6}01T0000_${date}T0000_sde.grib
 # calculate snow depth obs from swe kriging data
-cdo --eccodes aexprf,fmi-sde.instr fmi-smartmet-$ydate-sd-krg.grib grib/SMARTOBS_${date:0:6}01T0000_${date}T0000_sde.grib
+cdo --eccodes chparam,141.228,141.128 -mulc,0.01 fmi-smartmet-$ydate-sd-krg.grib grib/SMARTOBS_${date:0:6}01T0000_${date}T0000_sde.grib
 mv fmi-smartmet-* smartmet/
 sudo docker exec smartmet-server /bin/fmi/filesys2smartmet /home/smartmet/config/libraries/tools-grid/filesys-to-smartmet.cfg 0
