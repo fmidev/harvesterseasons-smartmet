e849e7791242e20e3150a3450e905c540a4bc103
Mikko Strahlendorff <mstrahl@gmail.com>
2021-02-20 10:57:44 +0000

    fix preload file and improve get-seasonal

--- bin/get-seasonal.sh
+++ bin/get-seasonal.sh
@@ -43,34 +43,34 @@ conda activate xr
 ## Make bias-adjustement
 ### adjust unbound variables
 [ -f ens/ec-sf_$year${month}_all-24h-eu-50.grib ] && ! [ -f ens/ec-${bsf}_$year${month}_unbound-24h-eu-50.grib ] && \
-seq 0 50 | parallel -j 16 --compress --tmpdir tmp/ cdo -O --eccodes aexprf,ec-sde.instr -ymonadd \
-    -remap,$era-eu-grid,ec-sf-$era-eu-weights.nc -selvar,2d,2t,rsn,sd,stl1,swvl1,swvl2 ens/ec-sf_$year${month}_all-24h-eu-{}.grib \
-    -selvar,2d,2t,rsn,sd,stl1,swvl1,swvl2 $era/$era-ecsf_2000-2019_unbound_bias_eu.grib \
+ seq 0 50 | parallel -j 16 --compress --tmpdir tmp/ cdo -O --eccodes aexprf,ec-sde.instr -ymonadd \
+    -remap,$era-eu-grid,ec-sf-$era-eu-weights.nc -selname,2d,2t,rsn,sd,stl1,swvl1,swvl2 ens/ec-sf_$year${month}_all-24h-eu-{}.grib \
+    -selname,2d,2t,rsn,sd,stl1,swvl1,swvl2 $era/$era-ecsf_2000-2019_unbound_bias_eu.grib \
     ens/ec-${bsf}_$year${month}_unbound-24h-eu-{}.grib || echo "NOT adj unbound seasonal forecast input missing or already produced"
 ### adjust wind
 [ -f ens/ec-sf_$year${month}_all-24h-eu-50.grib ] && ! [ -f ens/ec-${bsf}_$year${month}_bound-24h-eu-50.grib ] && \
-seq 0 50 | parallel -j 16 --compress --tmpdir tmp/ -q cdo -O --eccodes ymonmul \
-    -remap,$era-eu-grid,ec-sf-$era-eu-weights.nc -aexpr,'ws=sqrt(10u^2+10v^2);' -selvar,10u,10v ens/ec-sf_$year${month}_all-24h-eu-{}.grib \
-    -aexpr,'10u=ws;10v=ws;' -selvar,ws $era/$era-ecsf_2000-2019_bound_bias+varc_eu.grib \
+ seq 0 50 | parallel -j 16 --compress --tmpdir tmp/ -q cdo -O --eccodes ymonmul \
+    -remap,$era-eu-grid,ec-sf-$era-eu-weights.nc -aexpr,'ws=sqrt(10u^2+10v^2);' -selname,10u,10v ens/ec-sf_$year${month}_all-24h-eu-{}.grib \
+    -aexpr,'10u=ws;10v=ws;' -selname,ws $era/$era-ecsf_2000-2019_bound_bias+varc_eu.grib \
     ens/ec-${bsf}_$year${month}_bound-24h-eu-{}.grib || echo "NOT adj wind - seasonal forecast input missing or already produced"
 ### adjust evaporation and total precip or other accumulating variables
 [ -f ens/ec-sf_$year${month}_all-24h-eu-50.grib ] && ! [ -f ens/ec-${bsf}_$year${month}_acc-24h-eu-50.grib ] && \
-seq 0 50 | parallel -j 16 --compress --tmpdir tmp/ "cdo --eccodes -O mergetime -seltimestep,1 -selvar,e,tp ens/ec-sf_$year${month}_all-24h-eu-{}.grib \
-     -deltat -selvar,e,tp ens/ec-sf_$year${month}_all-24h-eu-{}.grib disacc-tmp-{}.grib && \
+ seq 0 50 | parallel -j 16 --compress --tmpdir tmp/ "cdo --eccodes -O mergetime -seltimestep,1 -selname,e,tp ens/ec-sf_$year${month}_all-24h-eu-{}.grib \
+     -deltat -selname,e,tp ens/ec-sf_$year${month}_all-24h-eu-{}.grib disacc-tmp-{}.grib && \
     cdo --eccodes ymonmul -remap,$era-eu-grid,ec-sf-$era-eu-weights.nc disacc-tmp-{}.grib \
-     -selvar,e,tp $era/$era-ecsf_2000-2019_bound_bias+varc_eu.grib \
+     -selname,e,tp $era/$era-ecsf_2000-2019_bound_bias+varc_eu.grib \
      ens/ec-${bsf}_$year${month}_disacc-24h-eu-{}.grib && \
     cdo --eccodes timcumsum ens/ec-${bsf}_$year${month}_disacc-24h-eu-{}.grib ens/ec-${bsf}_$year${month}_acc-24h-eu-{}.grib" && \
     rm disacc-tmp-*.grib || echo "NOT adj acc - seasonal forecast input missing or already produced"
 ## Make stl2,3,4 from stl1
 [ -f ens/ec-sf_$year${month}_all-24h-eu-50.grib ] && ! [ -f ens/ec-${bsf}_$year${month}_stl-24h-eu-50.grib ] && \
-seq 0 50 |parallel -j 16 --compress --tmpdir tmp/ -q cdo --eccodes -O ymonadd \
-    -aexpr,'stl2=stl1;stl3=stl1;stl4=stl1;' -remap,$era-eu-grid,ec-sf-$era-eu-weights.nc -selvar,stl1 ens/ec-sf_$year${month}_all-24h-eu-{}.grib \
-    -selvar,stl1,stl2,stl3,stl4 $era/$era-stls-diff+bias-climate-eu.grib \
+ seq 0 50 |parallel -j 16 --compress --tmpdir tmp/ -q cdo --eccodes -O ymonadd \
+    -aexpr,'stl2=stl1;stl3=stl1;stl4=stl1;' -remap,$era-eu-grid,ec-sf-$era-eu-weights.nc -selname,stl1 ens/ec-sf_$year${month}_all-24h-eu-{}.grib \
+    -selname,stl1,stl2,stl3,stl4 $era/$era-stls-diff+bias-climate-eu.grib \
     ens/ec-${bsf}_$year${month}_stl-24h-eu-{}.grib  || echo "NOT making stl levels 2,3,4 - seasonal forecast input missing or already produced"
 
 ## fix grib attributes
-[ -f ens/ec-${bsf}_$year${month}_unbound-24h-eu-50.grib ] && [ ! -f ens/ec-${bsf}_$year${month}_unbound-24h-eu-50-fixed.grib ] && \ 
+[ -f ens/ec-${bsf}_$year${month}_unbound-24h-eu-50.grib ] && [ ! -f ens/ec-${bsf}_$year${month}_unbound-24h-eu-50-fixed.grib ] && \
  seq 0 50 | parallel -j 16 grib_set -r -s centre=98,setLocalDefinition=1,localDefinitionNumber=15,totalNumber=51,number={} ens/ec-${bsf}_$year${month}_unbound-24h-eu-{}.grib \
     ens/ec-${bsf}_$year${month}_unbound-24h-eu-{}-fixed.grib || echo "NOT fixing unbound gribs attributes - no input or already produced"
 [ -f ens/ec-${bsf}_$year${month}_bound-24h-eu-50.grib ] && [ ! -f ens/ec-${bsf}_$year${month}_bound-24h-eu-50-fixed.grib ] && \
@@ -94,11 +94,5 @@ wait
 [ -f ec-sf_$year${month}_all-24h-eu.grib ] && cdo --eccodes aexprf,ec-sde.instr ec-sf-$year$month-all-24h.grib grib/ECSF-${year}${month}01T0000-all-24h.grib
 # produce forcing file for HOPS
 /home/smartmet/harvesterseasons-hops2smartmet/get-seasonal_hops.sh ${year} ${month}
-# old HOPS production 
-#[ -f grib/EC${bsf}_$year${month}01T0000_unbound-24h-eu.grib ] && cdo --eccodes -f nc2 merge -sellonlatbox,0,42,74,51 -selvar,2t,sde,swvl2 grib/EC${bsf}_$year${month}01T0000_unbound-24h-eu.grib \
-#    -sellonlatbox,0,42,74,51 grib/EC${bsf}_$year${month}01T0000_acc-24h-eu.grib -sellonlatbox,0,42,74,51 -selvar,stl2 grib/EC${bsf}_$year${month}01T0000_stl-24h-eu.grib \
- #   ../bin/harvester_code_hops/data/ecmwf/domains/scandi/fcast_ens/ec-sf-$year$month-z$bsf-24h-nordic.nc
 
-# RETIRED code...
-#grib_set  -s jScansPositively=0,numberOfForecastsInEnsemble=51 -w jScansPositively=1,numberOfForecastsInEnsemble=0 EC-SF_$year${month}01T0000_all-24h-euro+y.grib grib/EC-SF_$year${month}01T0000_all-24h-euro.grib
-sudo docker exec smartmet-server /bin/fmi/filesys2smartmet /home/smartmet/config/libraries/tools-grid/filesys-to-smartmet.cfg 0
+#sudo docker exec smartmet-server /bin/fmi/filesys2smartmet /home/smartmet/config/libraries/tools-grid/filesys-to-smartmet.cfg 0
