8b1cc35c9b1a585a55fd149c143f6b3c7d02af71
Mikko Strahlendorff <mstrahl@gmail.com>
2021-04-21 11:12:25 +0000

    fix get-swi

--- bin/get-swi-daily.sh
+++ bin/get-swi-daily.sh
@@ -4,7 +4,7 @@ conda activate xr
 if [[ $# -gt 0 ]]; then
     yday=`date -d $1 +%Y%m%d`
 else
-    yday=`date -d '10 days ago' +%Y%m%d`
+    yday=`date -d '14 days ago' +%Y%m%d`
 fi
 incoming=/home/smartmet/data/copernicus
 mkdir -p $incoming
@@ -21,26 +21,23 @@ file=${ncfile:0:-3}-swi.grib
 ceph="https://copernicus.data.lit.fmi.fi/land/eu_swi1km/$ncfile"
 
 #wget -q --method=HEAD $ceph && wget -q $ceph && upload=grb || 
-echo "Downloading from vito"; wget -q --random-wait $url && \
+[ ! -s "$ncfile" ] && echo "Downloading from vito" && wget -q --random-wait $url && \
      wget -q --random-wait $meta || res=NOT
 #nfile=${ncfile:0:-3}-swi_noise.tif
 #cog="${file:0:-4}_cog.tif"
 #ncog="${nfile:0:-4}_cog.tif"
+nc_ok=$(cdo xinfon $ncfile)
 
-if [ -s $ncfile ]
+if [ -z "$nc_ok" ]
 then
- cdo --eccodes -f grb -b P8 copy -chparam,-4,40.228,-8,41.228,-14,42.228,-16,43.228 -selname,SWI_005,SWI_015,SWI_060,SWI_100 $ncfile $file
- if [ "$res" == *NOT* ]
- then
-    echo "Downloading failed: $ncfile" 
- else 
+    echo "Downloading failed: $ncfile $url" 
+else 
+  cdo --eccodes -f grb -s -b P8 copy -chparam,-4,40.228,-8,41.228,-14,42.228,-16,43.228 -selname,SWI_005,SWI_015,SWI_060,SWI_100 $ncfile $file && \
     s3cmd put -q -P --no-progress $ncfile s3://copernicus/land/eu_swi1km/ &&\
      s3cmd put -q -P --no-progress $file s3://copernicus/land/eu_swi1km_grb/ &&\
-      s3cmd put -q -P --no-progress ${ncfile:0:-3}.xml s3://copernicus/land/eu_swi1km_meta/
-    rm $ncfile ${ncfile:0:-3}.xml
-    mv $file ../grib/SWI_${file:13:6}01T1200_${file:13:8}T${file:21:4}_swis.grib
- fi
-else
- echo "download failed: $url"
+       s3cmd put -q -P --no-progress ${ncfile:0:-3}.xml s3://copernicus/land/eu_swi1km_meta/
+    #rm $ncfile ${ncfile:0:-3}.xml
+    mv $file ../grib/SWI_${file:13:6}01T120000_${file:13:8}T${file:21:4}_swis.grib
 fi
+
 #sudo docker exec smartmet-server /bin/fmi/filesys2smartmet /home/smartmet/config/libraries/tools-grid/filesys-to-smartmet.cfg 0
