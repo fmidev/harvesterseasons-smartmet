72a534bd5bc458f86a7b1f7395c50e53b6f56e4b
Mikko Strahlendorff <mikko.strahlendorff@fmi.fi>
2025-06-27 05:20:50 +0000

    removing NaNdata from result

--- bin/run-xgb-predict-swi2-ecens.sh
+++ bin/run-xgb-predict-swi2-ecens.sh
@@ -30,11 +30,16 @@ echo 'shift laihv lailv swi2clim dates'
     cdo seldate,$DATE,$EDATE -shifttime,${diff}years SWIC_20000101T000000_2020_2015-2022_swis-ydaymean-nd-9km-fixed.grib ec-ens/SWIC_${year}${month}${day}T000000_2020_2015-2022_swis-ydaymean-nd-9km-fixed.grib || echo 'not shifting'
 #seldate,$DATE,$EDATE
 
+# fix grib attributes for laihv,lailv and swi2clim
+grib_set -r -s jScansPositively=0 ec-ens/ECC_${year}${month}${day}T000000_laihv-nd-day.grib ec-ens/ECC_${year}${month}${day}T000000_laihv-nd-day-fixed.grib
+grib_set -r -s jScansPositively=0 ec-ens/ECC_${year}${month}${day}T000000_lailv-nd-day.grib ec-ens/ECC_${year}${month}${day}T000000_lailv-nd-day-fixed.grib
+grib_set -r -s jScansPositively=0 ec-ens/SWIC_${year}${month}${day}T000000_2020_2015-2022_swis-ydaymean-nd-9km-fixed.grib ec-ens/SWIC_${year}${month}${day}T000000_2020_2015-2022_swis-ydaymean-nd-9km-fixed-fixed.grib
+
 input1=ec-ens/ensmems/ECENS_${year}${month}${day}T000000_${era}_sfc+sde-nd-{}.grib
 input2=ec-ens/ensmems/ec-ens_${DATE}_${era}_sfc-disacc-nd-{}.grib
-input3=ec-ens/ECC_${year}${month}${day}T000000_laihv-nd-day.grib
-input4=ec-ens/ECC_${year}${month}${day}T000000_lailv-nd-day.grib
-input5=ec-ens/SWIC_${year}${month}${day}T000000_2020_2015-2022_swis-ydaymean-nd-9km-fixed.grib
+input3=ec-ens/ECC_${year}${month}${day}T000000_laihv-nd-day-fixed.grib
+input4=ec-ens/ECC_${year}${month}${day}T000000_lailv-nd-day-fixed.grib
+input5=ec-ens/SWIC_${year}${month}${day}T000000_2020_2015-2022_swis-ydaymean-nd-9km-fixed-fixed.grib
 
 output=ec-ens/ECXENS_${year}${month}${day}_swi2_${era}_nd-out-{}.nc
 
@@ -43,21 +48,24 @@ echo 'start xgb predict'
 ! [ -s ec-ens/ECXENS_${year}${month}${day}_swi2_${era}_nd-out-50.nc ] && \
  seq 0 50 | parallel --tmpdir /home/ubuntu/data/tmp -j20 python /home/ubuntu/bin/xgb-predict-swi2-${era}-ecens.py $input1 $input2 $input3 $input4 $input5 $output || echo 'not predicting - already done'
 
-# netcdf to grib
+# netcdf to grib and remove first/last date (all nan)
 echo 'netcdf to grib'
 #[ -s ec-ens/ECXENS_${year}${month}${day}_swi2_${era}_nd-out-50.nc ] && ! [ -s ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-50.grib ] && \
-seq 0 50 | parallel --tmpdir /home/ubuntu/data/tmp cdo -b 16 -f grb2 copy -setparam,41.228.192 -setmissval,-9.e38 ec-ens/ECXENS_${year}${month}${day}_swi2_${era}_nd-out-{}.nc ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-{}.grib || echo "NO input or already netcdf to grib"
+seq 0 50 | parallel --tmpdir /home/ubuntu/data/tmp cdo -b 16 -f grb2 copy -setparam,41.228.192 -setmissval,-9.e38 -delete,timestep=1 -delete,timestep=-1 ec-ens/ECXENS_${year}${month}${day}_swi2_${era}_nd-out-{}.nc ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-{}.grib || echo "NO input or already netcdf to grib"
 
 # fix grib attributes
 echo 'fix grib attributes'
-#[ -s ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-50.grib ] && ! [ -s ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-50-fixed.grib ] && \
+[ -s ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-50.grib ] && ! [ -s ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-50-fixed.grib ] && \
 #seq 0 50 | parallel --tmpdir /home/ubuntu/data/tmp grib_set -r -s productDefinitionTemplateNumber=11,centre=86,totalNumber=51,number={} ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-{}.grib ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-{}-fixed.grib || echo "NOT fixing swi2 grib attributes - no input or already produced"
 seq 0 50 | parallel --tmpdir /home/ubuntu/data/tmp grib_set -r -s edition=1,setLocalDefinition=1,localDefinitionNumber=15,centre=98,totalNumber=51,number={} ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-{}.grib ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-{}-fixed.grib || echo "NOT fixing swi2 grib attributes - no input or already produced"
 
 # join ensemble members and move to grib folder
 echo 'join ensemble members and move to grib folder'
-#[ -s ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-50-fixed.grib ] && ! [ -s grib/ECXENS_${year}${month}${day}T000000_swi2-${era}-nd.grib ] && \
+[ -s ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-50-fixed.grib ] && ! [ -s grib/ECXENS_${year}${month}${day}T000000_swi2-${era}-nd.grib ] && \
 grib_copy ec-ens/ECXENS_${year}${month}${day}_swi2-${era}-nd-out-*-fixed.grib grib/ECXENS_${year}${month}${day}T000000_swi2-${era}-nd.grib || echo "NOT joining ens members - no input or already done"
 
 echo 'done'
 #wait 
+
+echo 'add to smartmet server'
+sudo docker exec smartmet-server /bin/fmi/filesys2smartmet /home/smartmet/config/libraries/tools-grid/filesys-to-smartmet.cfg 0
